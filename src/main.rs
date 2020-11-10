7fbd1b5b8f5b12d126ed1de9058243d0626daa2c
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2020-11-10 23:01:48 +0800

    stubbing out lexer and parser impls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-mod lexer {}
+mod lexer {
+    pub type Tokens = Vec<String>;
 
-mod parser {}
+    pub fn lex(_s: String) -> Tokens {
+        todo!();
+    }
+}
+
+mod parser {
+
+    #[allow(dead_code)]
+    pub enum Form {
+        List(Vec<Form>),
+        Boolean(bool),
+        String(String),
+        Integer(i64),
+        Double(f64),
+    }
+
+    pub type SyntaxTree = Form;
+
+    pub fn parse(_tokens: super::lexer::Tokens) -> SyntaxTree {
+        todo!();
+    }
+}
 
 fn read_string<R: std::io::Read>(mut reader: R) -> String {
     let mut s = String::new();
@@ -10,4 +32,6 @@ fn read_string<R: std::io::Read>(mut reader: R) -> String {
 
 fn main() {
     let s = read_string(std::io::stdin());
+    let tokens = lexer::lex(s);
+    let _ast = parser::parse(tokens);
 }
